444f3c26a971f8b6bea476786a6c4a9086d26e9d
Pavithran26 <137516256+Pavithran26@users.noreply.github.com>
2025-12-02 13:51:50 +0530

    Initial commit: Django project scaffold for Multi-Unit Organization Management System.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Multi-Unit Organization Management System
+
+This is a Django project scaffold for managing multi-unit organizations.
---
+++ multiunit_org/settings.py
@@ -0,0 +1,13 @@
+# Django settings for multiunit_org project.
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'apps.unit_app',
+]
+
+# Add more settings as required.
